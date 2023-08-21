8682d4774027b64151f3e3b9ea97c7985ee9d675
Wunsuol <89762330+Wunsuol@users.noreply.github.com>
2023-08-21 23:23:40 +0800

    Add files via upload

---
+++ Java_EX_TB_Base/Chapter_2/EX4/SelfAdd.java
@@ -0,0 +1,18 @@
+package EX4;
+
+public class SelfAdd {
+    public static void self() {
+        int j=2,i=3;
+        j*=i-=(i++);
+        System.out.println("j="+j+","+"i="+i);
+        j=2;i=3;
+        j*=i-=(++i);
+        System.out.println("j="+j+","+"i="+i);
+
+    }
+
+    public static void main(String args[]) {
+        SelfAdd.self();
+    }
+
+}
