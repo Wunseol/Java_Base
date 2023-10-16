a7ea2d1905d0a5e1ec74fb757638ceef94c845bb
Wunsuol <89762330+Wunsuol@users.noreply.github.com>
2023-10-16 23:19:41 +0800

    Add files via upload

---
+++ Java_EX_TB_Base/Chapter_7/Sample2/Sample_2.java
@@ -0,0 +1,17 @@
+package Sample2;
+
+public class Sample_2 {
+    static void throwsEx() throws ArithmeticException{
+        int []a=new int[5];
+        for(int i=0;i<a.length;i++){
+            a[i]=10/i;
+        }
+    }
+    public static void main(String[]args){
+        try{
+            Sample_2.throwsEx();
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }
+    }
+}
